8d83cf7734d0477b1d193f8e5e0fa22bc5ee03d9
maytham-onaissi <maytham_onaissi@hotmail.com>
2020-12-20 20:00:48 +0300

    Create Program.cs

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string characterName = "Ahmed";
+                string superPower1 = "Iron shield";
+                string superPower2 = "Night vision";
+                int age = 15;
+                float speed = 1.65F;
+                System.Console.WriteLine("My name is {0} and my super powers are {1} and {2}. My age is {3} and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+        }
+    }
+}
